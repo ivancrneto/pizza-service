78a3a92b9f65d1ae797030cdf82a66f7bab5cbb8
Ivan Neto <ivan.cr.neto@gmail.com>
2018-11-09 16:21:54 -0300

    models fix

--- api/models.py
+++ api/models.py
@@ -1,21 +1,24 @@
 from django.db import models
-from model_utils import Choices, TimeStampedModel
+from model_utils import Choices
+from model_utils.models import TimeStampedModel
 
 
 class Pizza(TimeStampedModel):
     SIZES = Choices(
-        (S30_CM, '30 cm'),
-        (S50_CM, '50 cm'),
+        ('30', 'S30_CM', '30 cm'),
+        ('50', 'S50_CM', '50 cm'),
     )
     size = models.CharField(
         choices=SIZES, default=SIZES.S30_CM, max_length=20)
 
 
 class Customer(TimeStampedModel):
-    name = models.CharField(max_lenght=40)
-    address = models.CharField(max_lenght=100)
+    name = models.CharField(max_length=40)
+    address = models.CharField(max_length=100)
 
 
 class Order(TimeStampedModel):
-    pizza = models.ForeignKey('api.Pizza')
-    customer = models.ForeignKey('api.Customer')
+    pizza = models.ForeignKey(
+        'api.Pizza', null=True, on_delete=models.SET_NULL)
+    customer = models.ForeignKey(
+        'api.Customer', null=True, on_delete=models.SET_NULL)
