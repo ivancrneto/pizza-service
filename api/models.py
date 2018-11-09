705e6669f8a4605466f824d94c7184987d340cf9
Ivan Neto <ivan.cr.neto@gmail.com>
2018-11-09 10:44:23 -0300

    added customer, order and pizza models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from model_utils import Choices, TimeStampedModel
 
-# Create your models here.
+
+class Pizza(TimeStampedModel):
+    SIZES = Choices(
+        (S30_CM, '30 cm'),
+        (S50_CM, '50 cm'),
+    )
+    size = models.CharField(
+        choices=SIZES, default=SIZES.S30_CM, max_length=20)
+
+
+class Customer(TimeStampedModel):
+    name = models.CharField(max_lenght=40)
+    address = models.CharField(max_lenght=100)
+
+
+class Order(TimeStampedModel):
+    pizza = models.ForeignKey('api.Pizza')
+    customer = models.ForeignKey('api.Customer')
